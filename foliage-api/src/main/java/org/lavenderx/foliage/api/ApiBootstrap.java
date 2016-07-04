86647bed5b2d5813573d64b7b6f7a51b619814fb
Baymax <dolphineor@gmail.com>
2016-07-04 22:16:36 +0800

    Add undertow as web server

--- foliage-api/src/main/java/org/lavenderx/foliage/api/ApiBootstrap.java
+++ foliage-api/src/main/java/org/lavenderx/foliage/api/ApiBootstrap.java
@@ -1,14 +1,36 @@
 package org.lavenderx.foliage.api;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
+import lombok.extern.slf4j.Slf4j;
+import org.lavenderx.foliage.api.web.WebServer;
 
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+@Slf4j
 public class ApiBootstrap {
 
-    public static void main(String[] args) {
-        final AbstractApplicationContext ctx =
-                new AnnotationConfigApplicationContext();
+    private static final String SERVER_PORT = "foliage.server.port";
 
-        Runtime.getRuntime().addShutdownHook(new Thread(ctx::close));
+    public static void main(String[] args) {
+        try {
+            int port;
+            if (System.getProperty(SERVER_PORT) == null) {
+                port = 8080;
+            } else {
+                port = Integer.parseInt(System.getProperty(SERVER_PORT));
+            }
+
+            WebServer webServer = new WebServer("FoliageServer", port).start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    webServer.destroy();
+                } catch (final Exception ex) {
+                    log.error("An exception occurred when destroying server", ex);
+                }
+            }));
+        } catch (IOException | ServletException ex) {
+            log.error("", ex);
+        }
     }
 }
