63685ce31478907eabc2525385601c076f14b5a8
Baymax <dolphineor@gmail.com>
2016-07-23 11:03:09 +0800

    Update code

--- foliage-rpc/src/main/kotlin/org/lavenderx/foliage/nettyrpc/proxy/InterceptorHandler.kt
+++
@@ -1,9 +0,0 @@
-package org.lavenderx.foliage.nettyrpc.proxy
-
-import java.lang.reflect.Method
-
-interface InterceptorHandler {
-
-    @Throws(Throwable::class)
-    fun invoke(proxy: Any, method: Method, vararg args: Any): Any
-}
--- foliage-rpc/src/main/kotlin/org/lavenderx/foliage/nettyrpc/proxy/RpcProxy.kt
+++ foliage-rpc/src/main/kotlin/org/lavenderx/foliage/nettyrpc/proxy/RpcProxy.kt
@@ -1,37 +1,33 @@
 package org.lavenderx.foliage.nettyrpc.proxy
 
-import javassist.util.proxy.MethodHandler
-import javassist.util.proxy.ProxyFactory
-import javassist.util.proxy.ProxyObject
+import net.sf.cglib.proxy.Enhancer
+import net.sf.cglib.proxy.MethodInterceptor
+import net.sf.cglib.proxy.MethodProxy
 import java.lang.reflect.Method
 
-/**
- * <a href="http://www.cnblogs.com/hujunzheng/p/5134478.html"></a>
- * <a href="http://cuishen.iteye.com/blog/421464"></a>
- */
-class RpcProxy : MethodHandler {
+class RpcProxy : MethodInterceptor {
+
+    private val enhancer = Enhancer()
+    fun getProxy(interfaceClass: Class<*>): Any {
+        enhancer.setSuperclass(interfaceClass)
+        enhancer.setCallback(this)
+        return enhancer.create()
+    }
+
 
     @Throws(Throwable::class)
-    override fun invoke(self: Any?, thisMethod: Method?, proceed: Method?, args: Array<out Any>?): Any {
-        throw UnsupportedOperationException("not implemented")
+    override fun intercept(obj: Any, method: Method, args: Array<out Any>, proxy: MethodProxy): Any {
+        val result: Any = proxy.invoke(obj, args)
+
+        return result
     }
 
     companion object {
-        private val proxyFactory = ProxyFactory()
+        private val rpcProxy = RpcProxy()
 
         @Suppress("UNCHECKED_CAST")
-        @Throws(Exception::class)
         fun <T : Any> create(interfaceClass: Class<T>): T {
-//            proxyFactory.superclass = interfaceClass
-            proxyFactory.interfaces = arrayOf(interfaceClass)
-
-            val dynamicClass = proxyFactory.createClass()
-            val newInstance = dynamicClass.newInstance() as T
-            (newInstance as ProxyObject).handler = MethodHandler { self, thisMethod, proceed, args ->
-                proceed.invoke(newInstance, args)
-            }
-
-            return newInstance
+            return rpcProxy.getProxy(interfaceClass) as T
         }
     }
 }
